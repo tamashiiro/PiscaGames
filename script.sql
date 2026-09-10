f5a497ef0c7335f6bba9ad29567c544e1009e9ae
Davi Tamashiro <davitamashiro30@gmail.com>
2026-09-10 09:48:54 -0300

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE projeto_backend_abdias;
+
+USE projeto_backend_abdias;
+
+CREATE TABLE login (
+    id_usuario IT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50)
+    login VARCHAR(50)
+    senha VARCHAR(10)
+);
